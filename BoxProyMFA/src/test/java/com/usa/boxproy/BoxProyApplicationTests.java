3700ff6c8072e1ea0f0cd0bc12ea751c61d454be
Maira F Amaya <Maira.F.Amaya@skf.com>
2022-10-04 17:14:20 -0500

    actualización status

--- BoxProyMFA/src/test/java/com/usa/boxproy/BoxProyApplicationTests.java
+++ BoxProyMFA/src/test/java/com/usa/boxproy/BoxProyApplicationTests.java
@@ -3,7 +3,7 @@ package com.usa.boxproy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BoxProyApplicationTests.class)
 class BoxProyApplicationTests {
 
     @Test
